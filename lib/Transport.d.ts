0221d8c03250646d4cdf13efd3ade17da7b62cfa
Brian Foster <bdfoster89@gmail.com>
2017-03-26 16:13:48 -0400

    Revert "add abstract class Transport"
    This reverts commit 2e430088acba235c116da7c7c2464d9760590127.

--- lib/Transport.d.ts
+++
@@ -1,9 +0,0 @@
-import { Entry } from './';
-export declare abstract class Transport {
-    constructor(options: Object);
-    abstract init(options: Object): any;
-    abstract send(entry: Entry): any;
-    open(): void;
-    close(): void;
-}
-export default Transport;
