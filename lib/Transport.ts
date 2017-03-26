68423fd94db2c0261da056b8676eb51b5716d0a7
Brian Foster <bdfoster89@gmail.com>
2017-03-26 16:15:16 -0400

    Add abstract Transport

---
+++ lib/Transport.ts
@@ -0,0 +1,19 @@
+import {Entry} from './';
+
+export abstract class Transport {
+  constructor(options: Object) {
+    this.init(options);
+  }
+
+  public abstract init(options: Object);
+
+  public abstract send(entry: Entry);
+
+  public open() {
+    return;
+  };
+  public close() {
+    return;
+  };
+}
+export default Transport;
