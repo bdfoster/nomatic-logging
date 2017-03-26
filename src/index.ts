4cfe1190a8d67c19f80b267d14990e7e7318c08a
Brian Foster <bdfoster89@gmail.com>
2017-03-26 15:17:53 -0400

    add type MessageTemplate, reorg file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-export interface Levels {
-  [key: string]: number;
-}
-
 export interface Entry {
   namespace: string;
   message: string;
   createdAt: Date;
   hostname: string;
 }
+
+export interface Levels {
+  [key: string]: number;
+}
+
+export type MessageTemplate = string;
