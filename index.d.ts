2e430088acba235c116da7c7c2464d9760590127
Brian Foster <bdfoster89@gmail.com>
2017-03-26 16:11:17 -0400

    add abstract class Transport

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import Logger from './lib/Logger';
+import { Instances } from './lib';
+export declare const instances: Instances;
+export declare function register(instance: Logger): void;
+export declare function instance(namespace: string): Logger;
