a87db5b92fc7ff5b1ff705262e155e4945846406
Brian Foster <bdfoster89@gmail.com>
2017-03-26 16:02:14 -0400

    remove Typescript compiler outputs

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-import Logger from './lib/Logger';
-import { Instances } from './lib';
-export declare const instances: Instances;
-export declare function register(instance: Logger): void;
-export declare function instance(namespace: string): Logger;
