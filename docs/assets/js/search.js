bfcfa1fd915881c9e2ac7200aef9532324ff7e31
Brian Foster <bdfoster89@gmail.com>
2017-03-30 13:57:04 -0400

    add typedoc-generated documentation

---
+++ docs/assets/js/search.js
@@ -0,0 +1,498 @@
+﻿var typedoc = typedoc || {};
+typedoc.search = typedoc.search || {};
+typedoc.search.data = {
+    "kinds": {
+        "32": "Variable",
+        "128": "Class",
+        "256": "Interface",
+        "512": "Constructor",
+        "1024": "Property",
+        "2048": "Method",
+        "65536": "Type literal",
+        "262144": "Accessor",
+        "2097152": "Object literal",
+        "4194304": "Type alias"
+    },
+    "rows": [{
+        "id": 0,
+        "kind": 256,
+        "name": "Entry",
+        "url": "interfaces/entry.html",
+        "classes": "tsd-kind-interface"
+    }, {
+        "id": 1,
+        "kind": 1024,
+        "name": "name",
+        "url": "interfaces/entry.html#name",
+        "classes": "tsd-kind-property tsd-parent-kind-interface",
+        "parent": "Entry"
+    }, {
+        "id": 2,
+        "kind": 1024,
+        "name": "level",
+        "url": "interfaces/entry.html#level",
+        "classes": "tsd-kind-property tsd-parent-kind-interface",
+        "parent": "Entry"
+    }, {
+        "id": 3,
+        "kind": 1024,
+        "name": "message",
+        "url": "interfaces/entry.html#message",
+        "classes": "tsd-kind-property tsd-parent-kind-interface",
+        "parent": "Entry"
+    }, {
+        "id": 4,
+        "kind": 1024,
+        "name": "date",
+        "url": "interfaces/entry.html#date",
+        "classes": "tsd-kind-property tsd-parent-kind-interface",
+        "parent": "Entry"
+    }, {
+        "id": 5,
+        "kind": 1024,
+        "name": "data",
+        "url": "interfaces/entry.html#data",
+        "classes": "tsd-kind-property tsd-parent-kind-interface",
+        "parent": "Entry"
+    }, {
+        "id": 6,
+        "kind": 32,
+        "name": "transport",
+        "url": "globals.html#transport",
+        "classes": "tsd-kind-variable"
+    }, {
+        "id": 7,
+        "kind": 256,
+        "name": "TransportOptions",
+        "url": "interfaces/transportoptions.html",
+        "classes": "tsd-kind-interface"
+    }, {
+        "id": 8,
+        "kind": 1024,
+        "name": "level",
+        "url": "interfaces/transportoptions.html#level",
+        "classes": "tsd-kind-property tsd-parent-kind-interface",
+        "parent": "TransportOptions"
+    }, {
+        "id": 9,
+        "kind": 128,
+        "name": "Transport",
+        "url": "classes/transport.html",
+        "classes": "tsd-kind-class"
+    }, {
+        "id": 10,
+        "kind": 1024,
+        "name": "level",
+        "url": "classes/transport.html#level",
+        "classes": "tsd-kind-property tsd-parent-kind-class",
+        "parent": "Transport"
+    }, {
+        "id": 11,
+        "kind": 512,
+        "name": "constructor",
+        "url": "classes/transport.html#constructor",
+        "classes": "tsd-kind-constructor tsd-parent-kind-class tsd-is-overwrite",
+        "parent": "Transport"
+    }, {
+        "id": 12,
+        "kind": 2048,
+        "name": "execute",
+        "url": "classes/transport.html#execute",
+        "classes": "tsd-kind-method tsd-parent-kind-class",
+        "parent": "Transport"
+    }, {
+        "id": 13,
+        "kind": 1024,
+        "name": "listeners",
+        "url": "classes/transport.html#listeners",
+        "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Transport"
+    }, {
+        "id": 14,
+        "kind": 1024,
+        "name": "maxListeners",
+        "url": "classes/transport.html#maxlisteners",
+        "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Transport"
+    }, {
+        "id": 15,
+        "kind": 2048,
+        "name": "on",
+        "url": "classes/transport.html#on",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Transport"
+    }, {
+        "id": 16,
+        "kind": 2048,
+        "name": "once",
+        "url": "classes/transport.html#once",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Transport"
+    }, {
+        "id": 17,
+        "kind": 2048,
+        "name": "push",
+        "url": "classes/transport.html#push",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Transport"
+    }, {
+        "id": 18,
+        "kind": 2048,
+        "name": "pop",
+        "url": "classes/transport.html#pop",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Transport"
+    }, {
+        "id": 19,
+        "kind": 2048,
+        "name": "emit",
+        "url": "classes/transport.html#emit",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Transport"
+    }, {
+        "id": 20,
+        "kind": 4194304,
+        "name": "TransportHandler",
+        "url": "globals.html#transporthandler",
+        "classes": "tsd-kind-type-alias"
+    }, {
+        "id": 21,
+        "kind": 65536,
+        "name": "__type",
+        "url": "globals.html#transporthandler.__type",
+        "classes": "tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported",
+        "parent": "TransportHandler"
+    }, {
+        "id": 22,
+        "kind": 256,
+        "name": "Levels",
+        "url": "interfaces/levels.html",
+        "classes": "tsd-kind-interface"
+    }, {
+        "id": 23,
+        "kind": 256,
+        "name": "LoggerOptions",
+        "url": "interfaces/loggeroptions.html",
+        "classes": "tsd-kind-interface"
+    }, {
+        "id": 24,
+        "kind": 1024,
+        "name": "level",
+        "url": "interfaces/loggeroptions.html#level",
+        "classes": "tsd-kind-property tsd-parent-kind-interface",
+        "parent": "LoggerOptions"
+    }, {
+        "id": 25,
+        "kind": 1024,
+        "name": "template",
+        "url": "interfaces/loggeroptions.html#template",
+        "classes": "tsd-kind-property tsd-parent-kind-interface",
+        "parent": "LoggerOptions"
+    }, {
+        "id": 26,
+        "kind": 1024,
+        "name": "transports",
+        "url": "interfaces/loggeroptions.html#transports",
+        "classes": "tsd-kind-property tsd-parent-kind-interface",
+        "parent": "LoggerOptions"
+    }, {
+        "id": 27,
+        "kind": 1024,
+        "name": "levels",
+        "url": "interfaces/loggeroptions.html#levels",
+        "classes": "tsd-kind-property tsd-parent-kind-interface",
+        "parent": "LoggerOptions"
+    }, {"id": 28, "kind": 128, "name": "Logger", "url": "classes/logger.html", "classes": "tsd-kind-class"}, {
+        "id": 29,
+        "kind": 2097152,
+        "name": "_levels",
+        "url": "classes/logger.html#_levels",
+        "classes": "tsd-kind-object-literal tsd-parent-kind-class tsd-is-private",
+        "parent": "Logger"
+    }, {
+        "id": 30,
+        "kind": 32,
+        "name": "trace",
+        "url": "classes/logger.html#_levels.trace",
+        "classes": "tsd-kind-variable tsd-parent-kind-object-literal",
+        "parent": "Logger._levels"
+    }, {
+        "id": 31,
+        "kind": 32,
+        "name": "debug",
+        "url": "classes/logger.html#_levels.debug",
+        "classes": "tsd-kind-variable tsd-parent-kind-object-literal",
+        "parent": "Logger._levels"
+    }, {
+        "id": 32,
+        "kind": 32,
+        "name": "info",
+        "url": "classes/logger.html#_levels.info",
+        "classes": "tsd-kind-variable tsd-parent-kind-object-literal",
+        "parent": "Logger._levels"
+    }, {
+        "id": 33,
+        "kind": 32,
+        "name": "warn",
+        "url": "classes/logger.html#_levels.warn",
+        "classes": "tsd-kind-variable tsd-parent-kind-object-literal",
+        "parent": "Logger._levels"
+    }, {
+        "id": 34,
+        "kind": 32,
+        "name": "error",
+        "url": "classes/logger.html#_levels.error",
+        "classes": "tsd-kind-variable tsd-parent-kind-object-literal",
+        "parent": "Logger._levels"
+    }, {
+        "id": 35,
+        "kind": 1024,
+        "name": "transports",
+        "url": "classes/logger.html#transports",
+        "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-protected",
+        "parent": "Logger"
+    }, {
+        "id": 36,
+        "kind": 1024,
+        "name": "children",
+        "url": "classes/logger.html#children",
+        "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-protected",
+        "parent": "Logger"
+    }, {
+        "id": 37,
+        "kind": 1024,
+        "name": "name",
+        "url": "classes/logger.html#name",
+        "classes": "tsd-kind-property tsd-parent-kind-class",
+        "parent": "Logger"
+    }, {
+        "id": 38,
+        "kind": 1024,
+        "name": "template",
+        "url": "classes/logger.html#template",
+        "classes": "tsd-kind-property tsd-parent-kind-class",
+        "parent": "Logger"
+    }, {
+        "id": 39,
+        "kind": 512,
+        "name": "constructor",
+        "url": "classes/logger.html#constructor",
+        "classes": "tsd-kind-constructor tsd-parent-kind-class tsd-is-overwrite",
+        "parent": "Logger"
+    }, {
+        "id": 40,
+        "kind": 262144,
+        "name": "levels",
+        "url": "classes/logger.html#levels",
+        "classes": "tsd-kind-accessor tsd-parent-kind-class",
+        "parent": "Logger"
+    }, {
+        "id": 41,
+        "kind": 2048,
+        "name": "serialize",
+        "url": "classes/logger.html#serialize",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-private",
+        "parent": "Logger"
+    }, {
+        "id": 42,
+        "kind": 2048,
+        "name": "configure",
+        "url": "classes/logger.html#configure",
+        "classes": "tsd-kind-method tsd-parent-kind-class",
+        "parent": "Logger"
+    }, {
+        "id": 43,
+        "kind": 2048,
+        "name": "create",
+        "url": "classes/logger.html#create",
+        "classes": "tsd-kind-method tsd-parent-kind-class",
+        "parent": "Logger"
+    }, {
+        "id": 44,
+        "kind": 2048,
+        "name": "find",
+        "url": "classes/logger.html#find",
+        "classes": "tsd-kind-method tsd-parent-kind-class",
+        "parent": "Logger"
+    }, {
+        "id": 45,
+        "kind": 2048,
+        "name": "log",
+        "url": "classes/logger.html#log",
+        "classes": "tsd-kind-method tsd-parent-kind-class",
+        "parent": "Logger"
+    }, {
+        "id": 46,
+        "kind": 2048,
+        "name": "get",
+        "url": "classes/logger.html#get",
+        "classes": "tsd-kind-method tsd-parent-kind-class",
+        "parent": "Logger"
+    }, {
+        "id": 47,
+        "kind": 2048,
+        "name": "register",
+        "url": "classes/logger.html#register",
+        "classes": "tsd-kind-method tsd-parent-kind-class",
+        "parent": "Logger"
+    }, {
+        "id": 48,
+        "kind": 2048,
+        "name": "use",
+        "url": "classes/logger.html#use",
+        "classes": "tsd-kind-method tsd-parent-kind-class",
+        "parent": "Logger"
+    }, {
+        "id": 49,
+        "kind": 1024,
+        "name": "listeners",
+        "url": "classes/logger.html#listeners",
+        "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Logger"
+    }, {
+        "id": 50,
+        "kind": 1024,
+        "name": "maxListeners",
+        "url": "classes/logger.html#maxlisteners",
+        "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Logger"
+    }, {
+        "id": 51,
+        "kind": 2048,
+        "name": "on",
+        "url": "classes/logger.html#on",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Logger"
+    }, {
+        "id": 52,
+        "kind": 2048,
+        "name": "once",
+        "url": "classes/logger.html#once",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Logger"
+    }, {
+        "id": 53,
+        "kind": 2048,
+        "name": "push",
+        "url": "classes/logger.html#push",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Logger"
+    }, {
+        "id": 54,
+        "kind": 2048,
+        "name": "pop",
+        "url": "classes/logger.html#pop",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Logger"
+    }, {
+        "id": 55,
+        "kind": 2048,
+        "name": "emit",
+        "url": "classes/logger.html#emit",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Logger"
+    }, {
+        "id": 56,
+        "kind": 256,
+        "name": "ConsoleOptions",
+        "url": "interfaces/consoleoptions.html",
+        "classes": "tsd-kind-interface"
+    }, {
+        "id": 57,
+        "kind": 1024,
+        "name": "template",
+        "url": "interfaces/consoleoptions.html#template",
+        "classes": "tsd-kind-property tsd-parent-kind-interface",
+        "parent": "ConsoleOptions"
+    }, {
+        "id": 58,
+        "kind": 1024,
+        "name": "level",
+        "url": "interfaces/consoleoptions.html#level",
+        "classes": "tsd-kind-property tsd-parent-kind-interface tsd-is-inherited",
+        "parent": "ConsoleOptions"
+    }, {
+        "id": 59,
+        "kind": 128,
+        "name": "Console",
+        "url": "classes/console.html",
+        "classes": "tsd-kind-class"
+    }, {
+        "id": 60,
+        "kind": 1024,
+        "name": "template",
+        "url": "classes/console.html#template",
+        "classes": "tsd-kind-property tsd-parent-kind-class",
+        "parent": "Console"
+    }, {
+        "id": 61,
+        "kind": 512,
+        "name": "constructor",
+        "url": "classes/console.html#constructor",
+        "classes": "tsd-kind-constructor tsd-parent-kind-class tsd-is-overwrite",
+        "parent": "Console"
+    }, {
+        "id": 62,
+        "kind": 2048,
+        "name": "execute",
+        "url": "classes/console.html#execute",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-overwrite",
+        "parent": "Console"
+    }, {
+        "id": 63,
+        "kind": 1024,
+        "name": "level",
+        "url": "classes/console.html#level",
+        "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Console"
+    }, {
+        "id": 64,
+        "kind": 1024,
+        "name": "listeners",
+        "url": "classes/console.html#listeners",
+        "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Console"
+    }, {
+        "id": 65,
+        "kind": 1024,
+        "name": "maxListeners",
+        "url": "classes/console.html#maxlisteners",
+        "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Console"
+    }, {
+        "id": 66,
+        "kind": 2048,
+        "name": "on",
+        "url": "classes/console.html#on",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Console"
+    }, {
+        "id": 67,
+        "kind": 2048,
+        "name": "once",
+        "url": "classes/console.html#once",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Console"
+    }, {
+        "id": 68,
+        "kind": 2048,
+        "name": "push",
+        "url": "classes/console.html#push",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Console"
+    }, {
+        "id": 69,
+        "kind": 2048,
+        "name": "pop",
+        "url": "classes/console.html#pop",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Console"
+    }, {
+        "id": 70,
+        "kind": 2048,
+        "name": "emit",
+        "url": "classes/console.html#emit",
+        "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+        "parent": "Console"
+    }, {"id": 71, "kind": 32, "name": "console", "url": "globals.html#console", "classes": "tsd-kind-variable"}]
+};
